ca4f92d97c8e2f37b0cdf93085a2b240d8cedcd6
Na <nakeith01@gmail.com>
2025-12-29 18:29:00 +0700

    Add files via upload

---
+++ engToJam.js
@@ -0,0 +1,882 @@
+const engToJam = {
+    "a": [
+        "a"
+    ],
+    "add": [
+        "add"
+    ],
+    "addition": [
+        "add"
+    ],
+    "any": [
+        "ainii"
+    ],
+    "and": [
+        "and",
+        "ond"
+    ],
+    "understand": [
+        "andrstan"
+    ],
+    "anyone": [
+        "aniwœn"
+    ],
+    "air": [
+        "arro"
+    ],
+    "plane": [
+        "arroflia"
+    ],
+    "aeroplane": [
+        "arroflia"
+    ],
+    "arse": [
+        "arss"
+    ],
+    "bottom": [
+        "arss"
+    ],
+    "apple": [
+        "azol"
+    ],
+    "astronomy": [
+        "azonom"
+    ],
+    "bag": [
+        "bagir"
+    ],
+    "backpack": [
+        "bagir"
+    ],
+    "bye": [
+        "bai"
+    ],
+    "goodbye": [
+        "bai"
+    ],
+    "back (informal)": [
+        "bakk"
+    ],
+    "music band": [
+        "band"
+    ],
+    "pass": [
+        "bas"
+    ],
+    "basic": [
+        "basik"
+    ],
+    "be": [
+        "bii"
+    ],
+    "biology": [
+        "biolot"
+    ],
+    "planet": [
+        "blonat"
+    ],
+    "blue": [
+        "blœ"
+    ],
+    "burst": [
+        "bost"
+    ],
+    "back": [
+        "bœk"
+    ],
+    "bro": [
+        "bro"
+    ],
+    "bread": [
+        "brœd"
+    ],
+    "place": [
+        "bwas"
+    ],
+    "location": [
+        "bwas"
+    ],
+    "day": [
+        "dai"
+    ],
+    "that": [
+        "dat"
+    ],
+    "but": [
+        "dho"
+    ],
+    "though": [
+        "dho"
+    ],
+    "although": [
+        "dho"
+    ],
+    "the": [
+        "di"
+    ],
+    "did": [
+        "did"
+    ],
+    "dictionary": [
+        "diksonri"
+    ],
+    "ten": [
+        "dira"
+    ],
+    "this": [
+        "dis"
+    ],
+    "digital": [
+        "dizital"
+    ],
+    "do": [
+        "do"
+    ],
+    "jump": [
+        "dodd"
+    ],
+    "dog": [
+        "dog"
+    ],
+    "down": [
+        "donn"
+    ],
+    "two": [
+        "dos"
+    ],
+    "drink": [
+        "drink"
+    ],
+    "far": [
+        "faarr"
+    ],
+    "friend": [
+        "fiind",
+        "habibi"
+    ],
+    "five": [
+        "fim"
+    ],
+    "body": [
+        "fisi"
+    ],
+    "physical": [
+        "fisi"
+    ],
+    "physics": [
+        "fisiks"
+    ],
+    "fire": [
+        "flaim"
+    ],
+    "flame": [
+        "flaim"
+    ],
+    "fly": [
+        "flia"
+    ],
+    "flight": [
+        "fliat"
+    ],
+    "focus": [
+        "fokos"
+    ],
+    "multiply": [
+        "fol"
+    ],
+    "-fold": [
+        "fol"
+    ],
+    "phone": [
+        "fon"
+    ],
+    "for": [
+        "for"
+    ],
+    "festival": [
+        "fostiwit"
+    ],
+    "four": [
+        "fœr"
+    ],
+    "freedom": [
+        "fridom"
+    ],
+    "from": [
+        "from"
+    ],
+    "frog": [
+        "frœk",
+        "kobkradd"
+    ],
+    "gay": [
+        "gaii",
+        "gati"
+    ],
+    "lesbian (informal)": [
+        "gaii"
+    ],
+    "guys": [
+        "gais"
+    ],
+    "nine": [
+        "gako"
+    ],
+    "hundred": [
+        "gali"
+    ],
+    "game": [
+        "gamm"
+    ],
+    "lesbian (formal)": [
+        "gati"
+    ],
+    "guide": [
+        "giid"
+    ],
+    "get": [
+        "git"
+    ],
+    "bring": [
+        "git"
+    ],
+    "good day": [
+        "gœdidag"
+    ],
+    "good morning": [
+        "gœdimorgin"
+    ],
+    "good night": [
+        "gœdisiz"
+    ],
+    "LGBTQ+": [
+        "gœlli"
+    ],
+    "grateful": [
+        "gratol"
+    ],
+    "thankful": [
+        "gratol"
+    ],
+    "grow": [
+        "grœ"
+    ],
+    "have": [
+        "haab"
+    ],
+    "hi": [
+        "hai"
+    ],
+    "hello": [
+        "hallœ"
+    ],
+    "house": [
+        "haos"
+    ],
+    "him": [
+        "haz"
+    ],
+    "here": [
+        "hiirr"
+    ],
+    "his": [
+        "hiiz"
+    ],
+    "he": [
+        "hiz"
+    ],
+    "happiness": [
+        "hobisiz"
+    ],
+    "happy": [
+        "hobiz"
+    ],
+    "use": [
+        "hosi"
+    ],
+    "using": [
+        "hosi"
+    ],
+    "how": [
+        "hœ"
+    ],
+    "I": [
+        "i"
+    ],
+    "it": [
+        "id",
+        "it"
+    ],
+    "if": [
+        "if"
+    ],
+    "eight": [
+        "iiti"
+    ],
+    "I'm": [
+        "ik"
+    ],
+    "economy": [
+        "ikonos"
+    ],
+    "economic": [
+        "ikonoset"
+    ],
+    "expand": [
+        "iksban"
+    ],
+    "excitement": [
+        "iksimit"
+    ],
+    "excite": [
+        "iksit"
+    ],
+    "excited": [
+        "iksitid"
+    ],
+    "eat": [
+        "ikt",
+        "itt"
+    ],
+    "me": [
+        "imii"
+    ],
+    "my": [
+        "imii"
+    ],
+    "myself": [
+        "imii"
+    ],
+    "in": [
+        "in"
+    ],
+    "independence (informal)": [
+        "indizandis"
+    ],
+    "innovation": [
+        "innowi"
+    ],
+    "international": [
+        "intirnaizono"
+    ],
+    "is": [
+        "is"
+    ],
+    "independent": [
+        "issra"
+    ],
+    "independence": [
+        "issranis"
+    ],
+    "east": [
+        "isst"
+    ],
+    "is (alt)": [
+        "ist"
+    ],
+    "yesterday": [
+        "itoldai"
+    ],
+    "its": [
+        "iz"
+    ],
+    "call": [
+        "kal"
+    ],
+    "camp": [
+        "kammo"
+    ],
+    "night": [
+        "kan"
+    ],
+    "chemistry": [
+        "kimistri"
+    ],
+    "cold": [
+        "kold"
+    ],
+    "computer": [
+        "kommbotar"
+    ],
+    "computation": [
+        "kommbotat"
+    ],
+    "computational": [
+        "kommbotat"
+    ],
+    "gonna": [
+        "konna"
+    ],
+    "continue": [
+        "kontinœ"
+    ],
+    "continuous": [
+        "kontinœli"
+    ],
+    "contest": [
+        "kontis"
+    ],
+    "curry": [
+        "korri"
+    ],
+    "correction": [
+        "korrot"
+    ],
+    "cool": [
+        "kœll"
+    ],
+    "nearby": [
+        "krosi"
+    ],
+    "laptop": [
+        "labt"
+    ],
+    "yes": [
+        "lah",
+        "ti",
+        "za"
+    ],
+    "lie": [
+        "laii"
+    ],
+    "congratulations": [
+        "lasœksan"
+    ],
+    "legend": [
+        "ligind"
+    ],
+    "legendary": [
+        "ligindo"
+    ],
+    "life": [
+        "liif"
+    ],
+    "like": [
+        "liik"
+    ],
+    "revision": [
+        "liwisi",
+        "riwisi"
+    ],
+    "low": [
+        "low"
+    ],
+    "little": [
+        "low"
+    ],
+    "reduce": [
+        "lowin"
+    ],
+    "love": [
+        "loz"
+    ],
+    "the (less common)": [
+        "le"
+    ],
+    "magic": [
+        "maazik"
+    ],
+    "magical": [
+        "maazikal"
+    ],
+    "math": [
+        "math"
+    ],
+    "mathematical": [
+        "mathi"
+    ],
+    "prestigious mathematical olympiad": [
+        "mathi narizbikkolim"
+    ],
+    "question indicator": [
+        "mi"
+    ],
+    "mean": [
+        "mian"
+    ],
+    "meet": [
+        "midt"
+    ],
+    "meetup": [
+        "midttaab"
+    ],
+    "middle": [
+        "midwi"
+    ],
+    "midday": [
+        "midwidai"
+    ],
+    "midnight": [
+        "midwikan"
+    ],
+    "cloud": [
+        "mika"
+    ],
+    "milk": [
+        "milk"
+    ],
+    "smartphone": [
+        "mobilfon"
+    ],
+    "money": [
+        "monniz"
+    ],
+    "morning": [
+        "mœrnin"
+    ],
+    "no": [
+        "nai"
+    ],
+    "not": [
+        "nai"
+    ],
+    "divide": [
+        "naifol"
+    ],
+    "negative": [
+        "naigi"
+    ],
+    "black": [
+        "naiwi"
+    ],
+    "nation": [
+        "naizon"
+    ],
+    "national": [
+        "naizono"
+    ],
+    "prestigious": [
+        "narizbikk"
+    ],
+    "prestigious contest": [
+        "narizbikkolim"
+    ],
+    "enjoyable": [
+        "nasœksan"
+    ],
+    "subtract": [
+        "nidi"
+    ],
+    "need": [
+        "niida"
+    ],
+    "new": [
+        "niw"
+    ],
+    "newbie": [
+        "niwbi"
+    ],
+    "none": [
+        "nol"
+    ],
+    "north": [
+        "nott"
+    ],
+    "up": [
+        "ob"
+    ],
+    "old": [
+        "od"
+    ],
+    "of": [
+        "of"
+    ],
+    "okay": [
+        "ok",
+        "okaii"
+    ],
+    "day before yesterday": [
+        "oktaldai"
+    ],
+    "Olympics": [
+        "olimmna"
+    ],
+    "on": [
+        "onn"
+    ],
+    "orbit": [
+        "orwiti"
+    ],
+    "or": [
+        "ot"
+    ],
+    "update": [
+        "otdat"
+    ],
+    "updated": [
+        "otdatid"
+    ],
+    "utmost": [
+        "ottimmo"
+    ],
+    "red": [
+        "radi"
+    ],
+    "radish": [
+        "radiz"
+    ],
+    "radius": [
+        "rados"
+    ],
+    "revolve": [
+        "riiwo"
+    ],
+    "relax": [
+        "riliks"
+    ],
+    "religion": [
+        "rilozi"
+    ],
+    "day after tomorrow": [
+        "rirondai"
+    ],
+    "rest": [
+        "risti"
+    ],
+    "rizz (singular)": [
+        "risz"
+    ],
+    "respect": [
+        "rizbik"
+    ],
+    "picture": [
+        "robzil"
+    ],
+    "tomorrow": [
+        "rondai"
+    ],
+    "tree": [
+        "rœn"
+    ],
+    "south": [
+        "saats"
+    ],
+    "say": [
+        "sai"
+    ],
+    "tell": [
+        "sai"
+    ],
+    "left": [
+        "saio"
+    ],
+    "right": [
+        "salo"
+    ],
+    "smart": [
+        "samad"
+    ],
+    "summit": [
+        "samid"
+    ],
+    "her": [
+        "saz"
+    ],
+    "chatting": [
+        "shatin"
+    ],
+    "she": [
+        "sid"
+    ],
+    "hers": [
+        "siid"
+    ],
+    "science": [
+        "siiz"
+    ],
+    "scientific": [
+        "siizi"
+    ],
+    "prestigious science olympiad": [
+        "siiz narizbikk olimmna"
+    ],
+    "sigma": [
+        "sikma"
+    ],
+    "six": [
+        "siks"
+    ],
+    "six seven (slang)": [
+        "siks-sion"
+    ],
+    "seven": [
+        "sion"
+    ],
+    "zero": [
+        "siro"
+    ],
+    "statement": [
+        "sitmir"
+    ],
+    "school": [
+        "skœl"
+    ],
+    "snatch": [
+        "snatz"
+    ],
+    "snatcher": [
+        "snazr"
+    ],
+    "sorry": [
+        "sord"
+    ],
+    "super": [
+        "sœitar"
+    ],
+    "merry": [
+        "sœksana"
+    ],
+    "should": [
+        "sœt"
+    ],
+    "star": [
+        "star"
+    ],
+    "statue": [
+        "stato"
+    ],
+    "still": [
+        "stil"
+    ],
+    "still water": [
+        "stillwassir"
+    ],
+    "stone": [
+        "ston"
+    ],
+    "tablet": [
+        "tabfon"
+    ],
+    "take": [
+        "takk"
+    ],
+    "million": [
+        "tamminno"
+    ],
+    "thousand": [
+        "tanno"
+    ],
+    "them": [
+        "taz"
+    ],
+    "thick": [
+        "thikk"
+    ],
+    "team": [
+        "tiim"
+    ],
+    "group": [
+        "tiim"
+    ],
+    "theirs": [
+        "tiiz"
+    ],
+    "relieved": [
+        "tirbrik"
+    ],
+    "teacher": [
+        "tishir"
+    ],
+    "exam": [
+        "tisti"
+    ],
+    "they": [
+        "tiz"
+    ],
+    "to": [
+        "to"
+    ],
+    "today": [
+        "toldai"
+    ],
+    "three": [
+        "tri"
+    ],
+    "want": [
+        "want"
+    ],
+    "what": [
+        "was"
+    ],
+    "water": [
+        "wassir"
+    ],
+    "we": [
+        "wii"
+    ],
+    "wide": [
+        "wiid"
+    ],
+    "wire": [
+        "wiir"
+    ],
+    "wireless": [
+        "wiirnai"
+    ],
+    "week": [
+        "wik"
+    ],
+    "weekend": [
+        "wikiin"
+    ],
+    "win": [
+        "win"
+    ],
+    "version": [
+        "wirsin"
+    ],
+    "west": [
+        "wisst"
+    ],
+    "white": [
+        "wit"
+    ],
+    "with": [
+        "witt"
+    ],
+    "hope": [
+        "wiz"
+    ],
+    "hopeful": [
+        "wizif"
+    ],
+    "unite": [
+        "woniit"
+    ],
+    "unity": [
+        "woniiti"
+    ],
+    "you": [
+        "wœ"
+    ],
+    "one": [
+        "wœn"
+    ],
+    "them (non-binary)": [
+        "zad"
+    ],
+    "charm": [
+        "zam"
+    ],
+    "jamal": [
+        "zamœl"
+    ],
+    "jamaling": [
+        "zamœlin"
+    ],
+    "just": [
+        "zas"
+    ],
+    "they (non-binary)": [
+        "zid"
+    ],
+    "their (non-binary)": [
+        "ziid"
+    ],
+    "chill": [
+        "zill"
+    ],
+    "yellow": [
+        "zilod"
+    ]
+};
